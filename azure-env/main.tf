c6796c21de7c43205136b150a12e94fc6bb2cf40
Mehedi <me69oshan@gmail.com>
2023-09-08 12:23:08 -0600

    nsg and one rule added

--- azure-env/main.tf
+++ azure-env/main.tf
@@ -23,3 +23,27 @@ resource "azurerm_subnet" "snet1" {
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.123.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "nsg1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_network_security_rule" "nsrule1" {
+  name                        = "nsrule1-nsg1"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg1.name
+}
