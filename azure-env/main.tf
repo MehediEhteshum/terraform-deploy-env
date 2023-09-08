86fa7e159d054a57780da7e73785d1d0608a6b73
Mehedi <me69oshan@gmail.com>
2023-09-08 12:22:01 -0600

    v net and sub net added

--- azure-env/main.tf
+++ azure-env/main.tf
@@ -5,3 +5,21 @@ resource "azurerm_resource_group" "rg" {
     environment = var.environment
   }
 }
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1-rg"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_subnet" "snet1" {
+  name                 = "snet1-vnet1-rg"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
