98e7d79b6079c25ddbea50d9015eb58ec6fa4173
Mehedi <me69oshan@gmail.com>
2023-09-09 20:58:58 -0600

    ssh key, vm, data source added

--- aws-env/main.tf
+++ aws-env/main.tf
@@ -64,3 +64,38 @@ resource "aws_security_group" "sg1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_key_pair" "key-auth1" {
+  key_name   = "key-rsa1"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "vm1" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.vm1-ami.id
+  key_name               = aws_key_pair.key-auth1.id
+  vpc_security_group_ids = [aws_security_group.sg1.id]
+  subnet_id              = aws_subnet.snet1.id
+  user_data              = file("customdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  # usage of provisioner is a last resort or
+  # can be used for simple non-destructive tasks
+  # not recommended for complex operations
+  # provisioner changes are not captured in terraform state
+  provisioner "local-exec" {
+    command = templatefile("${var.local-os}-ssh-script.tpl", {
+      hostname     = self.public_ip_address,
+      user         = "ubuntu",
+      identityfile = "~/.ssh/id_rsa"
+    })
+    interpreter = var.local-os == "windows" ? ["Powershell", "-Command"] : ["bash", "-c"]
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
