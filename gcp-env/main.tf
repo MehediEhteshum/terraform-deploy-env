1a2d53c3bb5fa1dae1f5a3dbee4e7676f530a7d2
Mehedi <me69oshan@gmail.com>
2023-09-10 20:17:38 -0600

    vnet & subnet added

--- gcp-env/main.tf
+++ gcp-env/main.tf
@@ -1,1 +1,11 @@
+resource "google_compute_network" "vnet1" {
+  name                    = "vnet1"
+  auto_create_subnetworks = false
+  routing_mode            = "GLOBAL"
+}
 
+resource "google_compute_subnetwork" "snet1" {
+  name          = "snet1-vnet1"
+  ip_cidr_range = "10.123.1.0/24"
+  network       = google_compute_network.vnet1.id
+}
